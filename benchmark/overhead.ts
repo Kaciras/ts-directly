d35f6a0734027351533a2e1035c27bd4a645faea
Kaciras <Kaciras@protonmail.com>
2024-07-06 09:47:31 +0800

    Test load and resolve overheads separately.

--- benchmark/overhead.ts
+++ benchmark/overhead.ts
@@ -3,34 +3,12 @@ import { defineSuite } from "esbench";
 import { load, resolve } from "../src/loader.ts";
 import { setCompiler } from "./helper.js";
 
-function createMockImport(specifier: string) {
-	const url = pathToFileURL(specifier).href;
-
-	function nextResolve() {
-		return { url, importAttributes: {} };
-	}
-
-	function nextLoad() {
-		return { format: "ts" as any, source: "" };
-	}
-
-	const ctx: any = {
-		conditions: ["node", "import"],
-		importAttributes: {},
-	};
-
-	return async () => {
-		const resolved = await resolve(specifier, ctx, nextResolve);
-		return load(resolved.url, ctx, nextLoad);
-	};
-}
-
 await setCompiler(code => code);
 
 /**
  * Measure the cost of the ESM hook (exclude compiler and the next hook).
  *
- * pnpm exec esbench --file overhead.ts
+ * Run benchmark: pnpm exec esbench --file overhead.ts
  */
 export default defineSuite({
 	params: {
@@ -38,6 +16,23 @@ export default defineSuite({
 	},
 	async setup(scene) {
 		const { file } = scene.params;
-		scene.benchAsync("load", createMockImport(file));
+		const url = pathToFileURL(file).href;
+
+		function nextResolve() {
+			return { url, importAttributes: {} };
+		}
+
+		function nextLoad() {
+			return { format: "ts" as any, source: "" };
+		}
+
+		const ctx: any = {
+			conditions: ["node", "import"],
+			importAttributes: {},
+			parentURL: pathToFileURL("module.ts").href,
+		};
+
+		scene.benchAsync("resolve", () => resolve(file, ctx, nextResolve));
+		scene.benchAsync("load", () => load(url, ctx, nextLoad));
 	},
 });
