1ff8f79557680f2a8a3bd0b675cff4aad329fbcd
Kaciras <Kaciras@protonmail.com>
2024-11-05 11:46:02 +0800

    chore

--- src/tsconfig.ts
+++ src/tsconfig.ts
@@ -2,14 +2,14 @@ import { join, resolve as resolvePath } from "path";
 import { fileURLToPath } from "url";
 import { parse, TSConfckCache, TSConfckParseResult } from "tsconfck";
 
-export interface TSConfigEntry extends TSConfckParseResult {
+interface TSConfigEntry extends TSConfckParseResult {
 	base: string;
 	maps?: PathAlias[];
 }
 
 export const tsconfckCache = new TSConfckCache<any>();
 
-const EMPTY: readonly string[] = [];
+const EMPTY_ARRAY: readonly string[] = [];
 
 /**
  * Parse the closest tsconfig.json, and normalize some options.
@@ -23,21 +23,21 @@ export async function getTSConfig(file: string) {
 	if (!result.tsconfigFile) {
 		return;
 	}
+	// Cache the normalize result speed up the function by 24%.
 	if (!result.base) {
 		const options = result.tsconfig.compilerOptions ??= {};
 		const { paths, baseUrl = "" } = options;
 
-		options.inlineSourceMap = true;
-		options.removeComments = true;
+		result.base = resolvePath(result.tsconfigFile, "..", baseUrl);
+		if (paths) {
+			result.maps = PathAlias.parse(result, paths);
+		}
 
 		// Avoid modify source path in the source map.
 		delete options.outDir;
 
-		if (paths) {
-			result.maps = PathAlias.parse(result, paths);
-		}
-		result.base = resolvePath(result.tsconfigFile, "..", baseUrl);
-
+		options.inlineSourceMap = true;
+		options.removeComments = true;
 		options.target &&= options.target.toLowerCase();
 		options.module &&= options.module.toLowerCase();
 	}
@@ -86,10 +86,10 @@ class PathAlias {
 		return templates.map(t => t.replace("*", s));
 	}
 
-	static parse({ root }: any, paths: Record<string, string[]>) {
+	static parse({ base }: TSConfigEntry, paths: Record<string, string[]>) {
 		const alias: PathAlias[] = [];
 		for (const [key, templates] of Object.entries(paths)) {
-			alias.push(new PathAlias(root, key, templates));
+			alias.push(new PathAlias(base, key, templates));
 		}
 		// Pattern with the longer prefix before any * takes higher precedence.
 		return alias.sort((a, b) => b.prefix.length - a.prefix.length);
@@ -110,16 +110,16 @@ export async function getAlias(id: string, parent?: string) {
 		!parent?.startsWith("file:") ||	  // tsconfig is only include local files.
 		parent.includes("/node_modules/") // Library should not use alias.
 	) {
-		return EMPTY;
+		return EMPTY_ARRAY;
 	}
 	const found = await getTSConfig(fileURLToPath(parent));
 	if (!found) {
-		return EMPTY; // The package of `parent` does not have tsconfig.
+		return EMPTY_ARRAY; // The package of `parent` does not have tsconfig.
 	}
 	const { tsconfig: { compilerOptions }, base, maps } = found;
 	const match = maps?.find(item => item.test(id));
 	if (match) {
 		return match.getPaths(id);
 	}
-	return compilerOptions.baseUrl ? [join(base, id)] : EMPTY;
+	return compilerOptions.baseUrl ? [join(base, id)] : EMPTY_ARRAY;
 }
